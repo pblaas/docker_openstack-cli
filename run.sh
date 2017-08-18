988e245eeaabfcd375792724840953299e648260
pblaas <patrick@kite4fun.nl>
2017-08-18 23:03:57 +0200

    default to bash with git branch support

--- run.sh
+++ run.sh
@@ -29,7 +29,7 @@ if [ -d "/root/.ssh2" ]; then
 fi	
 
 #exporting more usefull shell for bash.
-export PS1="\[\033[1;90m\][\$(date +%H%M)]\[\033[1;92m\][\[\033[1;31m\]\u\[\033[1;92m\]:\[\033[1;37m\]\w\[\033[1;92m\]]$\[\033[0m\] "
+export PS1="\[\033[1;90m\][\$(date +%H%M)]\[\033[1;92m\][\[\033[1;31m\]\u\[\033[1;92m\]:\[\033[1;37m\]\w\[\033[1;92m\]]\[\033[33m\]\$(parse_git_branch) \[\033[1;92m\]$\[\033[0m\] "
 
 function typewriter
 {
@@ -53,19 +53,25 @@ function typewriter2
     done
 }
 
-echo 'export LS_OPTIONS="--color=auto"' > /root/.bashrc
-echo 'alias ls="ls $LS_OPTIONS"' >> /root/.bashrc
+cat > /root/.bashrc <<'endmsg'
+LS_OPTIONS="--color=auto"
+alias ls="ls $LS_OPTIONS"
+test
+parse_git_branch() {
+     git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/'
+}
+endmsg
 
 #fork new shell which contain set variables.
 if [ "$1" ]; then
-	#exec ssh-agent /bin/bash -c "$1" 
-	exec ssh-agent /bin/zsh -c "$1" 
+	exec ssh-agent /bin/bash -c "$1" 
+	#exec ssh-agent /bin/zsh -c "$1" 
 else
         typewriter2  "The" .04
 	typewriter  " Future " .04
 	typewriter2  "is now" .04
 	typewriter  "!" .04
 	echo
-	#exec ssh-agent /bin/bash --rcfile /root/.bashrc
-	exec ssh-agent /bin/zsh
+	exec ssh-agent /bin/bash --rcfile /root/.bashrc
+	#exec ssh-agent /bin/zsh
 fi
